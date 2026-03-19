851ea57ab483fb810acab2dc55808c6c50f63531
Davit Tsertsvadze <d.tsertsvadze@webintelligence.de>
2026-03-19 15:05:32 +0400

    Initial project setup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieQuotesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+  
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; 
+
+
+namespace MovieQuotesAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string TitleEn { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string TitleKa { get; set; }
+
+        // Relationship: ერთი ფილმს შეიძლება ჰქონდეს ბევრი Quote
+        public List<Quote> Quotes { get; set; }
+    }
+}
